7ca12c52846384ab2e75937bf411b8566453207b
DeadTree <lhwltwc@163.com>
2023-04-02 19:54:02 +0800

    part1.2.1

--- src/keyOperator.java
+++ src/keyOperator.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class keyOperator {
 
     public static void Operatorinput()
     {
-        
+        Scanner s= new Scanner(System.in);
+        String in=s.nextLine();
+        System.out.println(in);
     }
 }
