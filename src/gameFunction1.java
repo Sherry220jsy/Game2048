5ec6c4c9a77d209f405ec9d5b52d736995f8b609
DeadTree <lhwltwc@163.com>
2023-04-02 16:48:07 +0800

    part1.1

---
+++ src/gameFunction1.java
@@ -0,0 +1,7 @@
+public class gameFunction1 {
+    public static void Display(int [][]a){
+        for (int i=0;i<a.length;i++)
+            for (int j=0;j<a[i].length;j++)
+                System.out.println(a[i][j]);
+    }
+}
