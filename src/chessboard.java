b55ed0638096d42011ec846865f3d778cbe226d2
DeadTree <lhwltwc@163.com>
2023-04-02 19:48:27 +0800

    part1.5：Display++

--- src/chessboard.java
+++ src/chessboard.java
@@ -1,6 +1,6 @@
 public class chessboard {
-    public int score;
-    public String status;
+    public static int score;
+    public static String status;
     public static int[][]chessboard;
 
     public int getScore() {
@@ -13,12 +13,11 @@ public class chessboard {
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
 
-    public int[][] getA() {
+    public int[][] getChessboard() {
         return chessboard;
     }
 
@@ -30,9 +29,14 @@ public class chessboard {
 
 
     public static void Display(){
+        System.out.println("| 分数："+score+"      "+"状态："+status+" |");
+        if (status!="Ready")
         for (int i=0;i<chessboard.length;i++) {
-            for (int j = 0; j < chessboard.length; j++)
-                System.out.print(chessboard[i][j]);
+            for (int j = 0; j < chessboard.length; j++) {
+                if (chessboard[i][j]!=0)
+                System.out.print(chessboard[i][j] + " ");            //忽略为0的位置
+                else System.out.print("  ");
+            }
             System.out.println();
         }
     }
